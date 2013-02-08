9f99076914e14b628d5b3e628311e2d34c22b9e1
Anish Ramaswamy <ramaswamyanish@gmail.com>
2013-02-09 05:06:51 +0530

    Cleaned up code a bit. Added some logging and error handling functionalities.

--- src/client.c
+++ src/client.c
@@ -1,29 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-
-#define MAX_BUFLEN 1024
-
-
-// set the port number and address of server below
-
-#define set_port(s) \
-	strcpy(s, "60000")
-
-#define set_server(s) \
-	strcpy(s, "127.0.0.1")
-	
-
+#include "../include/client.h"
 int main(){
 	struct addrinfo hints;
 	struct addrinfo *result;
 	char port_no[8], server_address[30], buffer[MAX_BUFLEN];
 	int status;
 	int socket_fd; // socket file descriptor
+
+	log_init(l, "../");
 	set_port(port_no);
 	set_server(server_address);
 	
@@ -36,18 +19,16 @@ int main(){
 	hints.ai_socktype = SOCK_STREAM;
 	
 	if((status = getaddrinfo(server_address, port_no, &hints, &result ))!=0){
-		printf("getaddrinfo() error : %s", gai_strerror(status));
-		exit(EXIT_FAILURE);
+		log_error(errno, gai_strerror(status));
 	}
 	
 	if((socket_fd = socket(result->ai_family, result->ai_socktype, result->ai_protocol)) == -1)	{
-		printf("Error creating socket to server! \n");
+		log_error(errno, "Error creating socket to server.");
 	}
 	
-	printf("%d ", result->ai_addrlen);
+	fprintf(l, "%d\n", result->ai_addrlen);
 	if(connect(socket_fd, result->ai_addr, result->ai_addrlen)==-1){
-		perror("Error connecting!\n");
-		exit(EXIT_FAILURE);	
+		log_error(errno, "Error connecting!\n");
 	}
 	// sending sample data to buffer
 	strcpy(buffer,"amalantony@gmail.com");
